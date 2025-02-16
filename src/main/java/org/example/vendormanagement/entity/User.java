246f038f0021439f432fff8022ed68fe7ebbf295
Abhishek Bharti <bhartiabhishek310@gmail.com>
2025-02-16 18:29:29 +0530

    Added permissionOverries to user data so that user's permissions can be overrided by the Super vendors

--- src/main/java/org/example/vendormanagement/entity/User.java
+++ src/main/java/org/example/vendormanagement/entity/User.java
@@ -8,7 +8,10 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -31,4 +34,6 @@ public class User {
     private String vendorId;
     private Set<String> roles = new HashSet<>(); // ["SUPER_VENDOR", "REGIONAL_VENDOR"]
     private ObjectId delegationId; // delegation received by this user
+    // user specific permission overrides (e.g., {"canManageBooking": false})
+    private Map<String, Boolean> permissionOverrides = new HashMap<>();
 }
